32db06ff764281d5504ce1901230b2cc51cc27f2
Jhully Aires <jhullyolires@gmail.com>
2022-03-10 11:23:34 -0300

    written with java.io

--- java-io/src/br/com/alura/java/io/test/TesteLeitura.java
+++ java-io/src/br/com/alura/java/io/test/TesteLeitura.java
@@ -1,27 +1,25 @@
 package br.com.alura.java.io.test;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class TesteLeitura {
 	public static void main(String[] args) throws IOException {
 
 		// Fluxo de entrada com arquivo
-		InputStream fis = new FileInputStream("lorem.txt");
-		Reader isr = new InputStreamReader(fis);
-		BufferedReader br = new BufferedReader(isr);
+		OutputStream fos = new FileOutputStream("lorem2.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
 		
-		String linha = br.readLine();		
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+		bw.newLine();
+		bw.newLine();
+		bw.write("Sed pellentesque enim vel dictum tempor. Phasellus vulputate");
 		
-		while(linha != null) {
-			System.out.println(linha);
-			linha = br.readLine();
-		}
-		
-		br.close();
+		bw.close();
 	}
 }

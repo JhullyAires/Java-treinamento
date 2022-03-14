78bae9a2db04894c8588c7c09dc62b97c2e73871
Jhully Aires <jhullyolires@gmail.com>
2022-03-14 17:45:42 -0300

    commit Scanners

---
+++ java-io/src/br/com/alura/java/io/test/TesteLeitura2.java
@@ -0,0 +1,38 @@
+package br.com.alura.java.io.test;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+	public static void main(String[] args) throws Exception {
+		Scanner scanner = new Scanner(new File("contas.csv")); 
+		
+		while(scanner.hasNextLine()) {
+			String linha = scanner.nextLine();
+//			System.out.println(linha);	
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular  = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+			
+			String valorFormatado = String.format(new Locale("pt", "BR"), "%s - %04d-%08d, %s: %08.2f",
+												tipoConta, agencia, numero, titular, saldo);
+			System.out.println(valorFormatado);
+			
+			linhaScanner.close();
+			
+//			String[] valores = linha.split(",");
+////			System.out.println(Arrays.toString(valores));
+//			System.out.println(valores[3]);
+		}
+		scanner.close();
+	}
+}

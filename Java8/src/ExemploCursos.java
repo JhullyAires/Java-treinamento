820e3f905ae1aa128be99d78b653587faedf1434
Jhully Aires <jhullyolires@gmail.com>
2022-03-23 16:27:42 -0300

    using api

---
+++ Java8/src/ExemploCursos.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Curso {
+	private String nome;
+	private int alunos;
+
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getAlunos() {
+		return alunos;
+	}
+}
+
+public class ExemploCursos {
+	public static void main(String[] args) {
+		List<Curso> cursos = new ArrayList<>();
+		cursos.add(new Curso("Pyton", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+
+		// METHOD REFERENCE
+		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		// LAMBDA
+//		cursos.forEach(c -> System.out.println(c.getNome()));
+
+		int sum = cursos.stream()
+			.filter(c -> c.getAlunos() > 100)
+//			.map(c -> c.getAlunos())   // ou: .map(Curso::getAlunos)
+//			.forEach(total -> System.out.println(total)) //ou: .forEach(System.out::println);
+			// OU
+			.mapToInt(Curso::getAlunos)
+			.sum();  
+
+		System.out.println(sum);
+		
+//		cursos = cursos.stream()
+//			.filter(c -> c.getAlunos() >= 100)
+//			.findAny()
+//			.ifPresent(c -> System.out.println(c.getNome()));
+		
+		cursos = cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.collect(Collectors.toList());
+		
+		cursos.stream()
+			.forEach(c -> System.out.println(c.getNome()));
+
+		cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			.collect(Collectors.toMap(
+					c -> c.getNome(), 
+					c -> c.getAlunos()))
+			.forEach((nome, alunos) -> System.out.println(nome + " tem " + alunos +" alunos."));
+		
+	}
+}
--- Java8/src/OrdenaStringsComJava8.java
+++ Java8/src/OrdenaStringsComJava8.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStringsComJava8 {
 	public static void main(String[] args) {
@@ -20,35 +21,36 @@ public class OrdenaStringsComJava8 {
 //				return 1;
 //			return 0;
 //		});
-		
+
 		// Lambda mais sucinta
 		// Lambda consegue ser convertido para uma interface funcional
 //		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
-		
+
 		// E também equivalente a:
-		palavras.sort(Comparator.comparing(s -> s.length()));  // significa algo como: palavras ordene comparando o tamanho do string
-		
+		palavras.sort(Comparator.comparing(s -> s.length())); // significa algo como: palavras ordene comparando o
+																// tamanho do string
+		// OU
+		palavras.sort(Comparator.comparing(String::length)); // significa algo como: palavras ordene comparando o
+																// tamanho do string
+
+		Function<String, Integer> funcao = String::length;
+		// EQUIVALENTES
+		Function<String, Integer> funcao2 = s -> s.length();
 
 		System.out.println(palavras);
 
 		// Lambda
 		palavras.forEach(s -> System.out.println(s));
-		
-		// Equivalente a:
-		Consumer<String> impressor = s -> System.out.println(s);
+
+//		// Equivalente a:
+//		Consumer<String> impressor = s -> System.out.println(s);
+//		palavras.forEach(impressor);
+
+		// E também equivalente a:
+		Consumer<String> impressor = System.out::println;
 		palavras.forEach(impressor);
-
-	}
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if (s1.length() < s2.length())
-			return -1;
-		if (s1.length() > s2.length())
-			return 1;
-		return 0;
+		
+		//
+		palavras.forEach(System.out::println);
 	}
 }

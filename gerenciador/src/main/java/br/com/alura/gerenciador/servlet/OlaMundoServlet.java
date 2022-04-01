6635ffb92a92ce5cc902718657949edbe238adf7
Jhully Aires <jhullyolires@gmail.com>
2022-04-01 15:05:30 -0300

    commit web fundamentals and the Servlet API

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OlaMundoServlet.java
@@ -0,0 +1,28 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//oi
+@WebServlet(urlPatterns = "/oi")  // mapeamento
+public class OlaMundoServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>ola mundo, parabens!</h1> <h3>vc escreveu o primeiro servlets.</h3>");
+		out.println("</body>");
+		out.println("</html>");
+
+		System.out.println("o servlet OlaMundoServlet foi chamado");
+	}
+	
+}
